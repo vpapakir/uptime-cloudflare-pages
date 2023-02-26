207aa12718b20ade046f83dcfcde56b0f6ff8e10
Evangelos Papakirykou <vpapakir@gmail.com>
2023-02-26 01:24:39 +0000

    play default deps

--- server.js
+++ server.js
@@ -92,14 +92,14 @@ app.emit('afterLastRoute', app);
 // Sockets
 var io = socketIo.listen(server);
 
-io.configure('production', function() {
+/*io.configure('production', function() {
   io.enable('browser client etag');
   io.set('log level', 1);
 });
 
 io.configure('development', function() {
   if (!config.verbose) io.set('log level', 1);
-});
+});*/
 
 CheckEvent.on('afterInsert', function(event) {
   io.sockets.emit('CheckEvent', event.toJSON());
